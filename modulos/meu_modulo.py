768e8ed6e82b1f1002e4dc076ebf114b8754f913
igordamiao <igordamiao0205@gmail.com>
2024-09-30 17:24:08 -0300

    2023

--- modulos/meu_modulo.py
+++ modulos/meu_modulo.py
@@ -1,5 +1,5 @@
 def saudacao(nome):
-        return f"Olá, {nome}"
+        return (f"Olá, {nome}")
 
 def dobro(numero):
     return numero * 2
--- modulos/modulo_terceiro.py
+++ modulos/modulo_terceiro.py
@@ -1,8 +1,8 @@
 # pip3 install requests https://docs.python.org/3/library/index.html, https://docs.python.org/3/library/index.html e https://pypi.org/
 
 print("\Importação e uso de um módulo terceiros")
-import requests
+import requests # type: ignore
 
 url = "https://www.example.com"
-response = requests.get(url)
+response = requests.get(url)    
 print(f"Solicitação HTTP para {url} retornou o status {response.status_code}")
